7322ca09d293b2db67fa23ba37540a55a719be3e
sayed <sabauwan.habib@gmail.com>
2024-03-23 15:51:43 -0400

    complete the all scenario ready to share

---
+++ src/test/java/net/meetsky/pages/ProfileInfoPage.java
@@ -0,0 +1,48 @@
+package net.meetsky.pages;
+
+import net.meetsky.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProfileInfoPage extends BasePage {
+
+    public ProfileInfoPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+
+
+    }
+    @FindBy(xpath = "//div[@id='expand']")
+    public WebElement logoutButton;
+
+
+    @FindBy(xpath = "//a[@class='primary profile__header__container__edit-button']")
+    public WebElement linkEditProfile;
+  //  @FindBy(xpath = "//a[contains(@class, 'edit')]")
+    //public WebElement linkEditProfile;
+
+    @FindBy(xpath = "//div[@class='user-status-menu-item__header-content-displayname']")
+    public WebElement profileView;
+
+   // @FindBy(xpath = "(//div[contains(.,'View profile')])[4]")
+    //public WebElement profileView;
+
+    @FindBy(xpath = "//input[@id='displayname']")
+    public WebElement fullName;
+
+    @FindBy(xpath = "//span[.='sayed Habib']")
+    public WebElement UserName;
+
+    @FindBy(xpath = "//input[@id='email']")
+public WebElement email;
+
+
+    @FindBy(xpath = "//select[@id='language']")
+public WebElement language;
+
+
+
+
+}
+
